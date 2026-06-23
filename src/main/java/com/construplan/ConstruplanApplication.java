f68395bf921326f7ad40e7c09d524070ac1cdfc4
Eduardo Principe <i202506387@cibertec.edu.pe>
2026-06-23 06:37:23 -0500

    Update Time en Aplicacion

--- src/main/java/com/construplan/ConstruplanApplication.java
+++ src/main/java/com/construplan/ConstruplanApplication.java
@@ -1,11 +1,21 @@
 package com.construplan;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ConstruplanApplication {
 
+	@PostConstruct
+	public void initializeTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConstruplanApplication.class, args);
 	}
